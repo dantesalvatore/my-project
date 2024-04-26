0928c9f4c2cb262168bd6e885d4beb4fad292a27
dantesalvatore <47885775+dantesalvatore@users.noreply.github.com>
2024-04-26 11:30:36 +0100

    first commit

---
+++ images.js
@@ -0,0 +1,6 @@
+
+const image = { imgOne: require(.5254830.jpg)
+}
+ 
+
+export default image;
--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import logo from './logo.svg';
+import Logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <img src={require('./images/tx.jpg')} alt="" /> 
+      <img src={Logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
